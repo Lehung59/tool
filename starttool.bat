7320b20846898a277e8d89979b173bc84591deda
hungnl <hungnl@vivas.vn>
2024-10-26 17:07:44 +0700

    init

--- starttool.bat
+++ starttool.bat
@@ -1,22 +1,22 @@
-start wt -w 0 nt -d "F:\_TOOL\banana-fix3" cmd /k "python banana2.py"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\banana-fix3" cmd /k "python banana2.py"
 
 timeout /t 10 /nobreak
-start wt -w 0 nt -d "F:\_TOOL\birdton_v2" cmd /k "node .\bird.js"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\birdton_v2" cmd /k "node .\bird.js"
 
 timeout /t 10 /nobreak
-start wt -w 0 nt -d "F:\_TOOL\clayton" cmd /k "node .\clayton.js"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\clayton" cmd /k "node .\clayton.js"
 
 timeout /t 10 /nobreak
-start wt -w 0 nt -d "F:\_TOOL\coubv23" cmd /k "node .\coub.js"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\coubv23" cmd /k "node .\coub.js"
 
 timeout /t 10 /nobreak
-start wt -w 0 nt -d "F:\_TOOL\freedog" cmd /k "node .\freedog.js"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\freedog" cmd /k "node .\freedog.js"
 
 timeout /t 10 /nobreak
-start wt -w 0 nt -d "F:\_TOOL\pip-fix2" cmd /k "node .\pip3.js"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\pip-fix2" cmd /k "node .\pip3.js"
 
 timeout /t 10 /nobreak
-start wt -w 0 nt -d "F:\_TOOL\rating" cmd /k "(echo y & timeout /t 1 & echo y & timeout /t 1 & echo y & timeout /t 1 & echo 10) | node .\rating.js"
+start wt -w 0 nt -d "C:\Users\nguye\Downloads\Telegram Desktop\tool>\rating" cmd /k "(echo y & timeout /t 1 & echo y & timeout /t 1 & echo y & timeout /t 1 & echo 10) | node .\rating.js"
 
 
 
